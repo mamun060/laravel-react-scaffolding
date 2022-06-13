c0fe430aedf0ce31754226bfe48971c8256573a2
mamun060 <obujraju420@gmail.com>
2022-06-13 17:23:18 +0600

    localization demo added with laravel, React scafolding

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,17 +1,78 @@
 // import './App.css';
 import { Link, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "flag-icon-css/css/flag-icons.min.css"
 import Test from './components/Test';
 import Home from './pages/Home';
+import { useTranslation } from 'react-i18next';
+import i18next from "i18next";
+import cookies from 'js-cookie';
+import { useEffect } from 'react';
+
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import { BsGlobe } from "react-icons/bs"
+
+const languages = [
+  {
+    code: 'en',
+    name:'English',
+    country_code:'gb',
+  },
+  {
+    code: 'bn',
+    name:'বাংলাদেশ',
+    country_code:'bd',
+  },
+  {
+    code: 'ar',
+    name: 'العربية',
+    country_code:'sa',
+    dir: 'rtl',
+  }
+];
 
 function App() {
+
+  const currentLanguageCode = cookies.get('i18next') || 'en'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || 'ltr'
+    document.title    = t('title')
+  }, [currentLanguage, t])
+
+
   return (
     <div className="App">
      <div>
-        <Link to='/'>Home</Link>
+        <Link to='/'>{ t('home') }</Link>
      </div>
      <div>
         <Link to='/test'> Click Me</Link>
      </div>
+
+     <DropdownButton
+        variant="link"
+        title={<BsGlobe/>}
+        id="input-group-dropdown-1"
+      >
+        <Dropdown.Item>
+        <span>{t('language')}</span>
+        </Dropdown.Item>
+        {
+          languages.map(({code, name, country_code})=>(
+            <Dropdown.Item key={country_code} 
+            onClick={() => i18next.changeLanguage(code)}
+              disabled={code === currentLanguageCode}
+            >
+              <span className={`flag-icon flag-icon-${country_code} mx-2`}></span>
+              {name}</Dropdown.Item>
+          ))
+        }
+      </DropdownButton>
+
       <Routes>
         <Route path='/' element={<Home />}  />
           <Route path='/test' element={<Test />} />
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Suspense }  from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
+import i18n, { use } from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+
+
+i18n
+.use(initReactI18next)
+.use(LanguageDetector)
+.use(HttpApi)
+.init({
+  supportedLngs:['en','bn','fr','ar'],
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false
+  },
+  detection: {
+    order: ['path','cookie', 'htmlTag', 'localStorage', 'sessionStorage', 'navigator', 'querystring', 'subdomain'],
+    caches: ['localStorage', 'cookie'],
+  },
+  backend: {
+    loadPath: '/locales/{{lng}}/translation.json',
+  },
+  // react:{
+  //   useSuspense: false
+  // }
+});
+
+
+const loadingMarkup = (
+    <div className="py-4 text-center">
+      <h3>Loading..</h3>
+    </div>
+  )
+
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App /> 
-    </BrowserRouter>,
+    <Suspense fallback={loadingMarkup}>
+        <BrowserRouter>
+            <App /> 
+        </BrowserRouter>
+    </Suspense>,
     document.getElementById('root')
 );
